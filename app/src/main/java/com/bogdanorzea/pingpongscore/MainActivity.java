33b36de160ba2f77c61e487e1eedd833777a0e29
Bogdan Orzea <bogdan.orzea@gmail.com>
2017-05-04 22:59:38 +0300

    Player names update using the EditText, extract congratulation message string & fixes a rotation bug

--- app/src/main/java/com/bogdanorzea/pingpongscore/MainActivity.java
+++ app/src/main/java/com/bogdanorzea/pingpongscore/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String ENABLED_ADD_POINTS = "ENABLED_ADD_POINTS";
     private static final String PLAYER_1_SCORE = "PLAYER_1_SCORE";
     private static final String PLAYER_2_SCORE = "PLAYER_2_SCORE";
     private static final String PLAYER_1_SETS = "PLAYER_1_SETS";
@@ -42,7 +45,34 @@ public class MainActivity extends AppCompatActivity {
         namePlayer1 = (EditText) findViewById(R.id.player1_name);
         namePlayer2 = (EditText) findViewById(R.id.player2_name);
 
-        // TODO update player name on submit
+        namePlayer1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                myGame.setPlayer1Name(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+        namePlayer2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                myGame.setPlayer2Name(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
 
         addPointPlayer1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,9 +80,11 @@ public class MainActivity extends AppCompatActivity {
                 myGame.addPointPlayer1();
                 updateScore();
                 if (myGame.hasGameEnded()) {
-                    //TODO return set player name using:
-                    // endGame(v, myGame.getPlayerNames()[0]);
-                    endGame(v, "Player 1");
+                    String name = myGame.getPlayerNames()[0];
+                    if (name == null || name.isEmpty()) {
+                        name = getString(R.string.player_one_name);
+                    }
+                    endGame(v, name);
                 }
             }
         });
@@ -62,9 +94,11 @@ public class MainActivity extends AppCompatActivity {
                 myGame.addPointPlayer2();
                 updateScore();
                 if (myGame.hasGameEnded()) {
-                    //TODO return set player name using:
-                    // endGame(v, myGame.getPlayerNames()[1]);
-                    endGame(v, "Player 2");
+                    String name = myGame.getPlayerNames()[1];
+                    if (name == null || name.isEmpty()) {
+                        name = getString(R.string.player_two_name);
+                    }
+                    endGame(v, name);
                 }
             }
         });
@@ -110,10 +144,13 @@ public class MainActivity extends AppCompatActivity {
             int p2Sets = savedInstanceState.getInt(PLAYER_2_SETS);
             String p1Name = savedInstanceState.getString(PLAYER_1_NAME);
             String p2Name = savedInstanceState.getString(PLAYER_2_NAME);
+            boolean enabledAddPoints = savedInstanceState.getBoolean(ENABLED_ADD_POINTS);
 
             myGame = new PingPongGame(p1Sets, p2Sets, p1Score, p2Score);
             myGame.setPlayer1Name(p1Name);
             myGame.setPlayer2Name(p2Name);
+            addPointPlayer1.setEnabled(enabledAddPoints);
+            addPointPlayer2.setEnabled(enabledAddPoints);
 
         } else {
             myGame = new PingPongGame();
@@ -122,6 +159,9 @@ public class MainActivity extends AppCompatActivity {
         updateName();
     }
 
+    /**
+     * Updates the on-screen player names
+     */
     private void updateName() {
         namePlayer1.setText(myGame.getPlayerNames()[0]);
         namePlayer2.setText(myGame.getPlayerNames()[1]);
@@ -131,7 +171,7 @@ public class MainActivity extends AppCompatActivity {
         // Display Congratulation window
         AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
 
-        builder.setMessage(playerName + " won the game!");
+        builder.setMessage(getString(R.string.congratulate_dialog_message, playerName));
 
         builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
 
@@ -147,6 +187,9 @@ public class MainActivity extends AppCompatActivity {
         addPointPlayer2.setEnabled(false);
     }
 
+    /**
+     * Updates the on-screen score for the game
+     */
     private void updateScore() {
         scorePlayer1.setText(Integer.toString(myGame.getScore()[0]));
         scorePlayer2.setText(Integer.toString(myGame.getScore()[1]));
@@ -163,6 +206,7 @@ public class MainActivity extends AppCompatActivity {
         outState.putInt(PLAYER_2_SETS, myGame.getSets()[1]);
         outState.putString(PLAYER_1_NAME, myGame.getPlayerNames()[0]);
         outState.putString(PLAYER_2_NAME, myGame.getPlayerNames()[1]);
+        outState.putBoolean(ENABLED_ADD_POINTS, addPointPlayer1.isEnabled() || addPointPlayer2.isEnabled());
 
         super.onSaveInstanceState(outState);
     }
